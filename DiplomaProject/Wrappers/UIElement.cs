3b871716f0355f9755ba5e5a1ae008e4efb8ee8f
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-08-09 13:24:56 +0200

    Adding Wrappers directory with corresponding classes

---
+++ DiplomaProject/Wrappers/UIElement.cs
@@ -0,0 +1,12 @@
+namespace DiplomaProject.Wrappers;
+
+public class UIElement : BaseElement
+{
+    public UIElement(By locator) : base(locator)
+    {
+    }
+
+    public UIElement(string locator) : base(locator)
+    {
+    }
+}
