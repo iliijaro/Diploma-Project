e2b1367c9e9796cb109e9168f809e16a2f18e804
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-08-09 13:20:49 +0200

    Adding whole Pages according to the Page Object Pattern

---
+++ DiplomaProject/BaseEntities/BasePage.cs
@@ -0,0 +1,24 @@
+namespace DiplomaProject.BaseEntities;
+
+public abstract class BasePage
+{
+    protected Actions actions = null!;
+    protected IWebDriver Driver { get; set; }
+    protected WaitService Wait { get; set; }
+    public abstract BasePage OpenPage();
+    public abstract bool IsPageOpened();
+    public abstract BasePage RefreshPage();
+    
+    protected BasePage(IWebDriver driver, Actions actions, WaitService wait)
+    {
+        Driver = driver;
+        Wait = wait;
+        this.actions = actions;
+    }
+    
+    protected BasePage(IWebDriver driver, WaitService wait)
+    {
+        Driver = driver;
+        Wait = wait;
+    }
+}
