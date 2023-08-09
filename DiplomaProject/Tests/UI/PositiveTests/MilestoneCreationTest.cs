12703d338f2161e6a1855741b790ca0c377294cd
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-08-09 14:06:35 +0200

    Adding all UI tests

---
+++ DiplomaProject/Tests/UI/PositiveTests/MilestoneCreationTest.cs
@@ -0,0 +1,25 @@
+namespace DiplomaProject.Tests.UI.PositiveTests;
+
+[TestFixture]
+public class MilestoneCreationTest : BaseTest
+{
+    [Test, Category("Positive cases"), 
+     Description("Test case to check that the new Milestone is created")]
+    [AllureSeverity(SeverityLevel.critical)]
+    [AllureOwner("Admin")]
+    [AllureSuite("Positive Cases Suit")]
+    [AllureSubSuite("GUI")]
+    [AllureTms("11")]
+    [AllureTag("Smoke")]
+
+    public void NewMilestoneCreation()
+    {
+        LoginPage.LogIn(UserAdmin);
+        
+        MilestonesPage.OpenPage()
+            .CreateRandomMilestone(MilestoneBuilder.CreateRandomMilestone);
+        
+        Assert.That(MilestonesPage.SuccessMessageIsDisplayed(),
+            Is.True, "The message about successful creation is not displayed");
+    }
+}
